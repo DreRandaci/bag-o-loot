e3766d1305ef3b23467a4e30c70efc3496382f23
Dre mf Randaci <drerandaci@gmail.com>
2018-01-17 13:46:57 -0600

    basic CLI prompts running

--- bagOLoot/Program.cs
+++ bagOLoot/Program.cs
@@ -6,7 +6,49 @@ namespace bagOLoot
     {
         static void Main(string[] args)
         {
-            
+
+            ChildRegister childRegister = new ChildRegister();
+            ConsoleKeyInfo enteredKey;
+
+            do
+            {
+                Child newChild = new Child();
+                Console.WriteLine("Enter the first name of a child");
+                newChild.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter the last name of a child");
+                newChild.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter the address of a child");
+                newChild.Address = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine($"Child Name: {newChild.FirstName} {newChild.LastName}");
+                Console.WriteLine($"Child Address: {newChild.Address}");
+                childRegister.AddChild(newChild);
+
+                Console.WriteLine("Press m to move on or enter to add another child");
+                enteredKey = Console.ReadKey();
+
+            } while (enteredKey.Key != ConsoleKey.M);
+
+            Console.WriteLine();
+            string selectedChild;
+            do
+            {
+                Console.WriteLine("Assign toy to which child?");
+                int count = 1;
+                foreach (Child child in childRegister.GetRegister())
+                {
+                    Console.WriteLine($"{count}. {child.FirstName} {child.LastName}");
+                    count++;
+                }
+                selectedChild = Console.ReadLine();
+
+            } while (enteredKey.Key != ConsoleKey.Escape);
+
+
+
         }
     }
 }
